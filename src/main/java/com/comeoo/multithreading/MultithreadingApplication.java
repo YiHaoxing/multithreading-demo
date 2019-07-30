cd39e374c7e84154762a542cf962aa72d0044033
YiHaoxing <m15670388498>
2019-07-30 11:42:16 +0800

    提交启动类

---
+++ src/main/java/com/comeoo/multithreading/MultithreadingApplication.java
@@ -0,0 +1,18 @@
+package com.comeoo.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YiHaoXing
+ * @version 1.0.0
+ * @className com.comeoo.multithreading.MultithreadingApplication
+ * @description TODO
+ * @date 2019/7/30 11:39
+ */
+@SpringBootApplication
+public class MultithreadingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadingApplication.class);
+    }
+}
